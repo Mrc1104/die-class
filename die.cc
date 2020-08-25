8ede000fdad12cac23b0ea99e42cda458c52ed11
Ryan2 <mrcconaway@gmail.com>
2020-08-25 14:10:27 -0400

    Added implementation file

---
+++ die.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "die.h"
+using namespace std;
+
+
+//Constructors
+Die::Die(){ //defaults to 6 sides
+ 	numSides = 6;
+ 	srand(time(NULL));
+ 	faceValue = rand() % numSides + 1;	
+}
+
+Die::Die(int newNumSides){
+	if(newNumSides > 1){
+			numSides = newNumSides;
+		}
+	else{
+		numSides = 6;
+		}
+		srand(time(NULL));
+		faceValue = rand() % numSides + 1; 
+}
+//Setters
+void Die::setNumSides(int newNumSides){
+	if(newNumSides > 1){
+		numSides = newNumSides;
+		}
+	}
+
+//getters
+int​​ Die::getFaceValue​(){​
+	return​ faceValue;
+	}
+int ​​Die::getNumSides​(){
+	​return​ numSides;
+	}
+//helper functions
+int​ Die::roll​(){ 
+	   faceValue = ​rand​() % numSides + ​1​;
+	   ​return​ faceValue;
+	   }
+	   
+ostream& ​operator​ <<(ostream &out, ​const​ Die &d){
+	    out << d.faceValue;​
+		return​ out
+	}
